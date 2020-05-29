a364ac7fdbe61afc4f35252a43d6054aba9d8558
1058603082 <54460898+1058603082@users.noreply.github.com>
2020-05-30 00:43:28 +0800

    Delete box.h

--- 1/box.h
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-class Box{
-	public :  void setLen(int length,int width,int height);
-	          int  getV();
-		  
-	private : int m_iLength,m_iWidth,m_iHeight;	
-};
